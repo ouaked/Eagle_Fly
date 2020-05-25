688928f46cd338129dd6ab056d3827796787221f
ouaked <40290234+ouaked@users.noreply.github.com>
2020-05-25 10:23:12 +0200

    Update Nature.java

--- Eagle fly/Nature.java
+++ Eagle fly/Nature.java
@@ -2,14 +2,14 @@ public class Nature {
 
     public static void main(String[] args) {
 
-        Penguin pingou = new Penguin("Pingou");
-        pingou.dive();
-        pingou.swimDown(3);
-        pingou.swimUp(1);
-        pingou.swimDown(4);
-        pingou.swimUp(5);
-        pingou.swimUp(1);
-        pingou.getOut();
+        //Penguin pingou = new Penguin("Pingou");
+        //pingou.dive();
+        //pingou.swimDown(3);
+       // pingou.swimUp(1);
+        //pingou.swimDown(4);
+        //pingou.swimUp(5);
+        //pingou.swimUp(1);
+        //pingou.getOut();
 
         Eagle hawkeye = new Eagle("Hawkeye");
         // TODO : uncomment the following code in order to test it
